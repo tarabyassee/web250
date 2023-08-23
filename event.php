727e062f55ac975ab23c927730508d91a676a3d4
Tara Byassee <tarabyassee@students.abtech.edu>
2023-08-23 19:21:00 -0400

    Completed event.php

---
+++ event.php
@@ -0,0 +1,25 @@
+<?php
+
+function check_capacity( $capacity, $attendees = 0) {
+  if ( $attendees < $capacity ) {
+    return 'There are still tickets available.';
+  }
+
+  return 'This event is sold out.';
+
+}
+// Array: key => [capacity, attendees];
+
+$venues = array( 
+  'Cantina' => [100, 20],  
+  'Dorsia' => [74, 74], 
+  'The Max'=> [98, 100], 
+  'MacLaren\'s' => [53, 127], 
+  'The Banana Stand' => [2, 0], 
+); 
+  
+  foreach ( $venues as $name => $numbers ) { 
+    echo $name . ' - ' . check_capacity($numbers[0], $numbers[1]) . '<br/>'; 
+}
+
+  ?>
