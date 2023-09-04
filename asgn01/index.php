d169d14d6ee0f55440b2bf5341a855e3b9df6563
Tara Byassee <tarabyassee@students.abtech.edu>
2023-09-04 16:38:05 -0400

    added an index file for my asgn01 folder

---
+++ asgn01/index.php
@@ -0,0 +1,16 @@
+<<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Index Page</title>
+  </head>
+  
+  <body>
+    <h1>Index Page</h1>
+
+    <ul>
+      <li><a href="bird-challenge/bird-challenge.php">Bird Challenge</a></li>
+      <li><a href="bike-challenge/challenge-01.php">Bike Challenge</a></li>
+    </ul>
+  </body>
+</html>
