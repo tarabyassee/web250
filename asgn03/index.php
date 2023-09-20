6577f1ae8af9c4ffa5c5651279c8abef299fc8e9
Tara Byassee <tarabyassee@students.abtech.edu>
2023-09-20 09:55:05 -0400

    finished index.php. Removed the reference to flying that no longer existed in the Bird.php file

--- asgn03/index.php
+++ asgn03/index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>asgn02 Inheritance</title>
+    <title>asgn03 - Static</title>
 </head>
 <body>
-<h1>Inheritance Examples</h1>
+<h1>Static Examples</h1>
 
 <?php 
     include 'Bird.php';
@@ -17,10 +17,6 @@
     $fly_catcher = new YellowBelliedFlyCatcher;
     echo '<p>The song of the ' . $fly_catcher->name . ' on breeding grounds is "' . $fly_catcher->song . '".</p>';
 
-    $kiwi = new Kiwi;
-    $kiwi->flying = "no";
-    echo "<p>The " . $fly_catcher->name . " " . $fly_catcher->can_fly() . ".</p>";
-    echo "<p>The " . $kiwi->name . " " . $kiwi->can_fly() . ".</p>";    
 
 ?>
     </body>
