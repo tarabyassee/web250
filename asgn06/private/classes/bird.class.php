d40d3bf6d5a61ce7e6d47e6effababa887637899
Tara Byassee <tarabyassee@students.abtech.edu>
2023-10-04 10:53:45 -0400

    added properties to the bird class

---
+++ asgn06/private/classes/bird.class.php
@@ -0,0 +1,13 @@
+<?php 
+  class Bird {
+    public $commonName;
+    public $habitat;
+    public $food;
+    public $nestPlacement;
+    public $behavior;
+    public $conservationId;
+    public $backyardTips;
+
+
+  }
+?>
