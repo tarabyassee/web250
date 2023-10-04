415ba0888e095f67e214c2bcb5de90bc743879d1
Tara Byassee <tarabyassee@students.abtech.edu>
2023-10-04 10:32:45 -0400

    made changes to the index.php file to include links to bird.php and about.php

---
+++ asgn06/public/index.php
@@ -0,0 +1,8 @@
+<?php require_once('../private/initialize.php'); ?>
+
+<div id="main">
+  <ul>
+    <li><a href="<?php echo urlFor('/bird.php'); ?>">View Our List of WNC Birds</a></li>
+    <li><a href="<?php echo urlFor('/about.php'); ?>">About Us</a></li>
+  </ul>
+</div>
