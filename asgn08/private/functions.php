8c9f6ee48a27b83ff9afca0f516792e58f23baa2
Tara Byassee <tarabyassee@students.abtech.edu>
2023-10-24 10:23:33 -0400

    Added the completed files from asgn06

---
+++ asgn08/private/functions.php
@@ -0,0 +1,35 @@
+<?php
+
+function urlFor($scriptPath) {
+  if($scriptPath[0] !='/') {
+    $scriptPath = "/" . $scriptPath;
+  }
+  return WWW_ROOT . $scriptPath;
+}
+
+function u($string="") {
+  return urlencode($string);
+}
+
+function rawU($string="") {
+  return rawurlencode($string);
+}
+
+function h($string="") {
+  return htmlspecialchars($string);
+}
+
+function redirectTo($location) {
+  header("Location: " . $location);
+  exit;
+}
+
+function isPostRequest() {
+  return $_SERVER['REQUEST_METHOD'] == 'POST';
+}
+
+function isGetRequest() {
+  return $_SERVER['REQUEST_METHOD'] == 'GET';
+}
+
+?>
