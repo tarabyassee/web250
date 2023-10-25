56f4561a64564b11ea0e973fca4cec5c748b28a9
Tara Byassee <tarabyassee@students.abtech.edu>
2023-10-25 09:24:48 -0400

    added a stylesheet folder and file, remove 'nest placement' and 'behavior' from table data

--- asgn08-active-birds/public/birds.php
+++ asgn08-active-birds/public/birds.php
@@ -13,8 +13,6 @@
         <th>Name</th>
         <th>Habitat</th>
         <th>Food</th>
-        <th>Nest Placement</th>
-        <th>Behavior</th>
         <th>Conservation</th>
         <th>Backyard Tips</th>
         <th>&nbsp;</th>
@@ -29,8 +27,6 @@ $birds = Bird::find_all();
         <td><?php echo h($bird->common_name); ?></td>
         <td><?php echo h($bird->habitat); ?></td>
         <td><?php echo h($bird->food); ?></td>
-        <td><?php echo h($bird->nest_placement); ?></td>
-        <td><?php echo h($bird->behavior); ?></td>
         <td><?php echo h($bird->conservation()); ?></td>
         <td><?php echo h($bird->backyard_tips); ?></td>
         <td><a href="detail.php?id=<?php echo $bird->id; ?>">"View"</a></td>
