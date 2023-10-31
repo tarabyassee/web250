029df94e4a84b8a78b2b24189582f58eb62b2fe6
Tara Byassee <tarabyassee@students.abtech.edu>
2023-10-31 13:44:01 -0400

    added the DatabaseObject parent class and bicycle class still works as expected.

--- asgn09-CRUD/private/classes/bicycle.class.php
+++ asgn09-CRUD/private/classes/bicycle.class.php
@@ -1,156 +1,11 @@
 <?php
 
-class Bicycle {
+class Bicycle extends DatabaseObject {
 
   // ----- START OF ACTIVE RECORD CODE ------
-  static protected $database;
+  static protected $table_name = 'bicycles';
+
   static protected $db_columns = ['id', 'brand', 'model', 'year', 'category', 'color', 'gender', 'price', 'weight_kg', 'condition_id', 'description'];
-
-  public $errors = [];
-
-  static public function set_database($database) {
-    self::$database = $database;
-  }
-
-  static public function find_by_sql($sql) {
-    $result = self::$database->query($sql);
-    if(!$result) {
-      exit("Database query failed.");
-    }
-
-    // results into objects
-    $object_array = [];
-    while($record = $result->fetch_assoc()) {
-      $object_array[] = self::instantiate($record);
-    }
-
-    $result->free();
-
-    return $object_array;
-  }
-
-  static public function find_all() {
-    $sql = "SELECT * FROM bicycles";
-    return self::find_by_sql($sql);
-  }
-
-  static public function find_by_id($id) {
-    $sql = "SELECT * FROM bicycles ";
-    $sql .= "WHERE id='" . self::$database->escape_string($id) . "'";
-    $obj_array = self::find_by_sql($sql);
-    if(!empty($obj_array)) {
-      return array_shift($obj_array);
-    } else {
-      return false;
-    }
-  }
-
-  static protected function instantiate($record) {
-    $object = new self;
-    // Could manually assign values to properties
-    // but automatically assignment is easier and re-usable
-    foreach($record as $property => $value) {
-      if(property_exists($object, $property)) {
-        $object->$property = $value;
-      }
-    }
-    return $object;
-  }
-
-  protected function validate() {
-    $this->errors = [];
-
-    if(is_blank($this->brand)) {
-      $this->errors[] = "Brand cannot be blank.";
-    }
-    if(is_blank($this->model)) {
-      $this->errors[] = "Model cannot be blank.";
-    }
-    return $this->errors;
-  }
-
-  //this is an instance method
-  protected function create(){
-    $this->validate();
-    if(!empty($this->errors)) { return false; }
-
-    $attributes = $this->sanitized_attributes();
-    $sql = "INSERT INTO bicycles (";
-    $sql .= join(', ', array_keys($attributes));
-    $sql .= ") VALUES ('";
-    $sql .= join("', '", array_values($attributes));
-    $sql .= "')";
-     $result = self::$database->query($sql);
-    if($result) {
-      $this->id = self::$database->insert_id;
-    }
-    return $result; 
-  }
-
-  protected function update(){
-    $this->validate();
-    if(!empty($this->errors)) { return false; }
-
-    $attributes = $this->sanitized_attributes();
-    $attribute_pairs = [];
-    foreach($attributes as $key => $value) {
-      $attribute_pairs[] = "{$key}='{$value}'";
-    }
-
-    $sql = "UPDATE bicycles SET ";
-    $sql .= join(', ', $attribute_pairs);
-    $sql .= " WHERE id='" . self::$database->escape_string($this->id) . "' ";
-    $sql .= "LIMIT 1";
-    $result = self::$database->query($sql);
-    return $result;
-  }
-
-  public function save() {
-    //a new record will not have an id yet
-    if(isset($this->id)) {
-      return $this->update();
-    } else {
-      return $this->create();
-    }
-  }
-
-  public function merge_attributes($args=[]) {
-    foreach($args as $key => $value) {
-      if(property_exists($this, $key) && !is_null($value)) {
-        $this->$key = $value;
-      }
-    }
-  }
-
-//properties which have database columns, excluding id
-  public function attributes() {
-    $attributes = [];
-    foreach(self::$db_columns as $column) {
-      if($column == 'id') { continue; }
-      $attributes[$column] = $this->$column;
-    }
-    return $attributes;
-  }
-
-  protected function sanitized_attributes() {
-    $sanitized = [];
-    foreach($this->attributes() as $key => $value) {
-      $sanitized[$key] = self::$database->escape_string($value);
-    }
-    return $sanitized;
-  }
-
-  public function delete() {
-    $sql = "DELETE FROM bicycles ";
-    $sql .= "WHERE id='" . self::$database->escape_string($this->id) . "' ";
-    $sql .= "LIMIT 1";
-    $result = self::$database->query($sql);
-    return $result;
-
-    //after deleting the instance of the object will still exist even though the db record does not. this can be useful as in:
-    //echo $user->first_name . " was deleted"
-    //we cant call update
-  }
 
   // ----- END OF ACTIVE RECORD CODE ------
 
@@ -225,6 +80,18 @@ class Bicycle {
     } else {
       return "Unknown";
     }
+  }
+
+  protected function validate() {
+    $this->errors = [];
+
+    if(is_blank($this->brand)) {
+      $this->errors[] = "Brand cannot be blank.";
+    }
+    if(is_blank($this->model)) {
+      $this->errors[] = "Model cannot be blank.";
+    }
+    return $this->errors;
   }
 
 }
